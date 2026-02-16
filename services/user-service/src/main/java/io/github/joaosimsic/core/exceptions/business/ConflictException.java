21e87e868c0ca015e2570bbaea2ff87b9843b7cb
joao <joao.simsic@4x4brasil.com.br>
2026-02-16 11:24:57 -0300

    Revert "fix: conflicts"
    This reverts commit bf871d59c7c035cc8679a670ea6731a267c2cb6d, reversing
    changes made to 8562a2d2f863ca7c9b79b11ad341a9a83ece05be.

--- services/user-service/src/main/java/io/github/joaosimsic/core/exceptions/business/ConflictException.java
+++ services/user-service/src/main/java/io/github/joaosimsic/core/exceptions/business/ConflictException.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:services/user-service/src/main/java/io/github/joaosimsic/core/exceptions/business/ConflictException.java
 package io.github.joaosimsic.core.exceptions.business;
-========
-package com.example.message.core.exceptions.business;
->>>>>>>> origin/main:src/main/java/com/example/message/core/exceptions/business/ConflictException.java
 
 import io.github.joaosimsic.core.exceptions.abstracts.BusinessException;
 
